c2a158df2ef7fd8efc72d761e32711965b760127
Laurent Doguin <laurent.doguin@couchbase.com>
2016-07-21 11:40:15 +0200

    add ImageFromDockerFileTest

---
+++ src/test/java/com/couchbase/BuildDockerImageExampleTest.java
@@ -0,0 +1,51 @@
+package com.couchbase;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.cluster.ClusterManager;
+import com.couchbase.client.java.env.CouchbaseEnvironment;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+
+import static org.junit.Assert.assertTrue;
+
+;
+
+/**
+ * Created by ldoguin on 15/07/16.
+ */
+public class BuildDockerImageExampleTest {
+
+    @ClassRule
+    public static GenericContainer couchbase =
+            new GenericContainer(
+                    new ImageFromDockerfile("mycouchbase").withFileFromClasspath("Dockerfile", "Dockerfile")
+                    .withFileFromClasspath("scripts/dummy.sh","scripts/dummy.sh")
+                            .withFileFromClasspath("scripts/entrypoint.sh","scripts/entrypoint.sh")
+                            .withFileFromClasspath("scripts/run","scripts/run")
+            )
+                    .withExposedPorts(8091, 8092, 8093, 8094, 11207, 11210, 11211, 18091, 18092, 18093)
+                    .waitingFor(new CouchbaseWaitStrategy());
+
+
+    @Test
+    public void beerBucketTest() throws InterruptedException {
+        CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder()
+                .bootstrapCarrierDirectPort(couchbase.getMappedPort(11210))
+                .bootstrapCarrierSslPort(couchbase.getMappedPort(11207))
+                .bootstrapHttpDirectPort(couchbase.getMappedPort(8091))
+                .bootstrapHttpSslPort(couchbase.getMappedPort(18091))
+                .queryPort(couchbase.getMappedPort(8093))
+                .build();
+        CouchbaseCluster cc = CouchbaseCluster.create(env);
+        ClusterManager cm = cc.clusterManager("Administrator", "password");
+        assertTrue(cm.hasBucket("beer-sample"));
+        Bucket bucket = cc.openBucket("beer-sample");
+        assertTrue(bucket.exists("21st_amendment_brewery_cafe"));
+        bucket.close();
+    }
+
+}
